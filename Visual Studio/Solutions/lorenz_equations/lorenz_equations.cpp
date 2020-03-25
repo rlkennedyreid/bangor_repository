236b07e6c50b7ec1c715ccffd42c401a070346c1
Robin Kennedy-Reid <rlkennedyreid@protonmail.com>
2020-03-25 23:28:25 +0000

    Lorenze equation stuff, struggling to get bifurcation diagram

--- Visual Studio/Solutions/lorenz_equations/lorenz_equations.cpp
+++ Visual Studio/Solutions/lorenz_equations/lorenz_equations.cpp
@@ -1,26 +1,16 @@
-#include "lorenz_equations.h"
+//#include "lorenz_equations.h"
 
-std::array<double, 3> lorenz(std::array<double, 3> input)
-{
-	constexpr double sigma = 10.0;
-	constexpr double gamma = 28.0;
-	constexpr double beta = 8.0 / 3.0;
-
-	std::array<double, 3> output{ {0.0, 0.0, 0.0} };
-
-	output[0] = sigma * (input[1] - input[0]);
-	output[1] = input[0] * (gamma - input[2]) - input[1];
-	output[3] = input[0] * input[1] - beta * input[2];
-
-	return output;
-}
-
-void rungeKutta(std::array<double, 3> input, double step_size)
-{
-	std::array<std::array<double, 3>, 4> k_vals{ { {0.0, 0.0, 0.0},{0.0, 0.0, 0.0},{0.0, 0.0, 0.0},{0.0, 0.0, 0.0} } };
-
-	for (int i{ 0 }; i < 4; i++)
-	{
-		
-	}
-}
+//std::array<double, 3> lorenz(std::array<double, 3> input)
+//{
+//	constexpr double sigma = 10.0;
+//	constexpr double gamma = 28.0;
+//	constexpr double beta = 8.0 / 3.0;
+//
+//	std::array<double, 3> output{ {0.0, 0.0, 0.0} };
+//
+//	output[0] = sigma * (input[1] - input[0]);
+//	output[1] = input[0] * (gamma - input[2]) - input[1];
+//	output[3] = input[0] * input[1] - beta * input[2];
+//
+//	return output;
+//}
