5464cbb0dc661f8b10ab895b947cb1d1047434c8
rlkennedyreid <rlkennedyreid@protonmail.com>
2020-06-18 20:04:36 +0100

    Some unpushed changes, renames and documentation

--- MATLAB/OFDM_signal_generation/utilities/quantiseSignal.m
+++ MATLAB/OFDM_signal_generation/utilities/quantiseSignal.m
@@ -1,20 +1,24 @@
 function [quantised_signal] = quantiseSignal(SIGNAL, NUM_QUANTISATION_BITS)
-%QUANTISESIGNAL Summary of this function goes here
-%   Detailed explanation goes here
+%QUANTISESIGNAL Function to quantise a given signal into discrete levels
+%   The number of levels is 2^NUM_QUANTISATION_BITS. The min and max level
+%   is always the min and max of the original signal. Levels are evenly
+%   spaced.
 
-    assert(NUM_QUANTISATION_BITS > 0);
+    assert(NUM_QUANTISATION_BITS > 0, 'Number of quantisation bits must be a positive integer');
+    
     num_quant_levels = 2^NUM_QUANTISATION_BITS;
-
-%     SIGNAL = -100 + (100*2)*rand(1e6,1);
 
     minimum_val = min(SIGNAL);
     maximum_val = max(SIGNAL);
-
-    quant_spacing = abs((maximum_val-minimum_val)/(num_quant_levels-1));
-
-    codebook = minimum_val:quant_spacing:maximum_val;
-    partition = codebook(1:(end-1)) + quant_spacing/2;
     
-    [~,quantised_signal] = quantiz(SIGNAL,partition,codebook);
+    % For N levels, there are N-1 spaces
+    quant_spacing = abs((maximum_val-minimum_val)/(num_quant_levels-1));
+    
+    % partitions give the crossover value between mapping to corresponding
+    % elements in codebook
+    codebook = minimum_val:quant_spacing:maximum_val;
+    partitions = codebook(1:(end-1)) + quant_spacing/2;
+    
+    [~,quantised_signal] = quantiz(SIGNAL,partitions,codebook);
 end
 
--- MATLAB/OFDM_signal_generation/utilities/timestampString.m
+++ MATLAB/OFDM_signal_generation/utilities/timestampString.m
@@ -1,4 +1,8 @@
 function timestamp_string = timestampString()
+%TIMESTAMP_STRING Function to produce a formatted, second-accurate
+%timestamp string
+%   Used to timestamp saved figures, and make sure we don't accidentally
+%   overwrite
 
     timestamp_string = datestr(now,'yymmdd_HHMMSS');
 
